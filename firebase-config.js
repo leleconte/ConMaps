233e9c529d9050e680dd59ede5f96eb944e5c45d
leleconte <contepierraffaele@gmail.com>
2026-06-24 23:18:30 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBpuRZb1c9981lu8uuKvO79_zDaWP0DDQk",
+  authDomain: "conmaps-d3905.firebaseapp.com",
+  projectId: "conmaps-d3905",
+  storageBucket: "conmaps-d3905.firebasestorage.app",
+  messagingSenderId: "720327863050",
+  appId: "1:720327863050:web:2b3d7cb0ec255deffda8f3",
+  measurementId: "G-X3J1WJEFV9"
+};
